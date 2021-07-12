6e7bbd2445f15cfd2a5053c1e5874b3e9c041a11
nekosenko <brija.main@gmail.com>
2021-07-12 13:25:22 +0100

    ex00 done

--- M00/ex00/megaphone.cpp
+++ M00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 13:14:34 by mbrija            #+#    #+#             */
-/*   Updated: 2021/07/11 18:36:29 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/07/12 13:24:48 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,24 @@
 
 using namespace std;
 
-int len(string str)
-{
-	return (int)str.size();
-}
-
 int main(int ac, char **av)
 {
-	string str[ac];
+	int i = 1, j = 0;
+	
 	
 	if (ac <= 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-
-	for (int i = 0; i < ac; i++)
-	   str[i] = av[i];
-	   
-	for (int i = 1; i < ac - 1; i++)
-	{
-		printf("%d \n", len(str[i]));
-		//cout << len(str[i]) << endl;
-		// for(int j = 0; i < len(str[i]) ;j++)
-		// 	cout << str[i][j] << endl;
-	}
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	
+	while (av[i])
+	{	
+		j = 0;
+		while(av[i][j])
+		{
+			cout << (char)toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	cout << endl;
 	return 0;
 }
