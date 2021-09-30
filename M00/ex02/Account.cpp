15ec67ca3a0fd195b7b01748120463e42c22d276
Mohammed Brija <brija.main@gmail.com>
2021-09-30 12:24:31 +0100

    init 02

---
+++ M00/ex02/Account.cpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 12:07:13 by mbrija            #+#    #+#             */
+/*   Updated: 2021/09/30 12:23:58 by mbrija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+Account::Account( void )
+{
+
+}
+Account::Account( int initial_deposit )
+{
+
+}
+Account::~Account( void )
+{
+    
+}
+
+int Account::getNbAccounts()
+{
+    
+}
+int	    Account::getTotalAmount( void )
+{
+    
+}
+int	    Account::getNbDeposits( void )
+{
+    
+}
+int	    Account::getNbWithdrawals( void )
+{
+    
+}
+void    Account::displayAccountsInfos( void )
+{
+    
+}
+void	Account::makeDeposit( int deposit )
+{
+    
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+    
+}
+int		Account::checkAmount( void ) const
+{
+    
+}
+void	Account::displayStatus( void ) const
+{
+
+}
